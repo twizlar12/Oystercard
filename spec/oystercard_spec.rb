0ce9ac6d4b6e41bbc866fc200222acecb1ee5bf9
Chan <chanpreetsingh974@gmail.com>
2020-05-04 16:24:30 +0100

    Test for argument when topping up & method to pass test

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,5 +4,13 @@ describe Oystercard do
   it "Freshly initialized card has a balance of 0 by default" do
     expect(subject.balance).to eq(0)
   end
+
+  it "Top up requires an argument" do
+    expect(subject).to respond_to(:top_up).with(1).argument
+  end
+
 end
+
+
+
 
